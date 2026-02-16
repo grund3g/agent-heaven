b592af7363d57a1fd4a0d61efa43823e2ec9c1b5
Simon Schnabl <kontakt@simonschnabl.com>
2026-02-16 21:25:13 +0100

    feat: add default commit & push actions
    (cherry picked from commit 1f7ede3ead2cd47ec38e24450d457163278a0b34)

--- src/preload.ts
+++ src/preload.ts
@@ -132,9 +132,8 @@ contextBridge.exposeInMainWorld("agentHeaven", {
     });
     return res;
   },
-  checkoutsSuggestCommitMessage: async (jobId, opts) => {
-    const p = opts && typeof opts === "object" ? opts : {};
-    const res = await invokeOk("checkouts:suggestCommitMessage", { jobId, forceEnglish: !!p.forceEnglish });
+  checkoutsSuggestCommitMessage: async (jobId) => {
+    const res = await invokeOk("checkouts:suggestCommitMessage", { jobId });
     return res && typeof res.suggestion === "string" ? res.suggestion : "";
   },
 
